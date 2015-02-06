46ae362b9a0cb6e53d727fa70fed53dbbb5d392b
shashankgurudu <shashank233@gmail.com>
2015-02-07 00:18:46 +0530

    all files

---
+++ app.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function() {
+  console.log("Hello World!");
+   
+     document.getElementById("branch").onchange = function() {
+            console.log("hi");
+            console.log(a.value);
+            
+            window.open('cse.html','_self',false);
+               
+    };
+  
+});
